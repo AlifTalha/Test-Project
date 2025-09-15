0da67f64551109cb226e8d53568d4bbb2a14b900
ALIF HOSSAIN TALHA <hossainalif696@gmail.com>
2025-09-15 12:50:00 +0600

    new1 file

--- event-management-frontend/src/api/axios.js
+++ event-management-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend URL
+  baseURL: "https://test-project-1-riyv.onrender.com/api", 
 });
 
 export default api;
